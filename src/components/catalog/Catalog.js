5daa1e3214a8276a0ef822ca9ef4d4b78b52b969
Stalloyde <stalloyde93@gmail.com>
2023-07-02 00:07:40 +0800

    Add and style catalog page. WIP: cards for products

--- src/components/catalog/Catalog.js
+++ src/components/catalog/Catalog.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import Header from '../header/Header';
+import './Catalog.css';
 
 const Catalog = () => (
-  <>
+  <div className='catalog-container'>
     <Header />
-    <div>SHOPPING CATALOG HERE</div>
-  </>
+    <div class='brands'>
+      <ul>
+        Filter by:
+        <li>All brands</li>
+        <li>PRS</li>
+        <li>Gibson</li>
+        <li>Ibanez</li>
+        <li>Schecter</li>
+        <li>Kiesel</li>
+        <li>Fender</li>
+        <li>Suhr</li>
+      </ul>
+    </div>
+
+    <div className='catalog-content'>
+      <div class='products'>
+        <div>product 1</div>
+        <div>product 2</div>
+        <div>product 3</div>
+        <div>product 4</div>
+        <div>product 5</div>
+        <div>product 6</div>
+        <div>product 7</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+        <div>product 8</div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Catalog;
