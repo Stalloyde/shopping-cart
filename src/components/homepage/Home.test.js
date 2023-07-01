08527b9d3514fdd24c6b1a4eaf1be3337a0de24e
Stalloyde <stalloyde93@gmail.com>
2023-07-01 16:23:45 +0800

    Test if View Our Collection button links to correct path

--- src/components/homepage/Home.test.js
+++ src/components/homepage/Home.test.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Router, MemoryRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { act } from 'react-dom/test-utils';
 import Home from './Home';
-import { MemoryRouter } from 'react-router-dom';
 
 describe('Home component', () => {
-  it('renders correct homepage', () => {
+  it('renders the hompeage', () => {
     render(
       <MemoryRouter>
         <Home />
       </MemoryRouter>
     );
+
     expect(screen.getByTestId('homepage')).toMatchSnapshot();
+  });
+
+  it('change current path to catalog when button is clicked', async () => {
+    const user = userEvent.setup();
+    const history = createMemoryHistory({ initialEntries: ['/'] });
+    render(
+      <Router location={history.location} navigator={history}>
+        <Home />
+      </Router>
+    );
+    expect(history.location.pathname).toBe('/');
+    const button = screen.getByText('View Our Collection');
+    await act(async () => user.click(button));
+    expect(history.location.pathname).toBe('/catalog');
   });
 });
