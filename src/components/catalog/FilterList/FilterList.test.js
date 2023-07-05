3765c3e4ce8ef2da0551332ff30a800c25c24146
Stalloyde <stalloyde93@gmail.com>
2023-07-05 12:30:31 +0800

    Add tests for FilterLis component

--- src/components/catalog/FilterList/FilterList.test.js
+++ src/components/catalog/FilterList/FilterList.test.js
@@ -1,1 +1,46 @@
-//test if filter list is correct
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import FilterList from './FilterList';
+
+describe('Filter List', () => {
+  const guitars = [
+    {
+      brand: 'PRS',
+    },
+    {
+      brand: 'PRS',
+    },
+    {
+      brand: 'Gibson',
+    },
+    {
+      brand: 'Cort',
+    },
+    {
+      brand: 'Deluxe',
+    },
+  ];
+
+  it('filtered brands do not repeat', () => {
+    const brandArray = guitars.map((item) => {
+      return item.brand;
+    });
+
+    const uniqueBrands = brandArray.filter((brand, index) => {
+      return brandArray.indexOf(brand) === index;
+    });
+
+    const isArrayUnique = (array) => new Set(array).size !== array.length;
+
+    expect(isArrayUnique(uniqueBrands)).toBe(false);
+  });
+
+  it('All unique brands are rendered correctly', () => {
+    render(<FilterList guitars={guitars} />);
+    expect(screen.getByText('All brands')).toBeInTheDocument();
+    guitars.forEach((item) => {
+      expect(screen.getByText(item.brand)).toBeInTheDocument();
+    });
+  });
+});
