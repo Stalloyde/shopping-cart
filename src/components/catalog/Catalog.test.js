a6ab836bfe38a604d8d59e3f8b87d1c1dad0ee10
Stalloyde <stalloyde93@gmail.com>
2023-07-08 14:00:43 +0800

    Add test for filtering brands to render

--- src/components/catalog/Catalog.test.js
+++ src/components/catalog/Catalog.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Card from './Card/Card';
+import FilterList from './FilterList/FilterList';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 
 describe('Catalog Card', () => {
   const guitars = [
@@ -27,6 +30,13 @@ describe('Catalog Card', () => {
       width: '100%',
     },
     {
+      brand: 'Ibanez',
+      model: 'Ibanez AR520HFM-VLS - Violin Burst 2',
+      price: 300,
+      height: '100%',
+      width: '100%',
+    },
+    {
       brand: 'Schecter',
       model: 'Schecter Avenger Exotic - Spalted Maple',
       price: 100,
@@ -35,7 +45,7 @@ describe('Catalog Card', () => {
     },
   ];
 
-  it('Number of times Card is rendered is according to array length', () => {
+  it('Initial render renders all items in guitars array', () => {
     render(
       <>
         {guitars.map((item) => (
@@ -57,5 +67,57 @@ describe('Catalog Card', () => {
     buttons.forEach((item) => {
       expect(item).toBeInTheDocument();
     });
+  });
+
+  it('on filter - array contains only selected brands', async () => {
+    const setState = jest.fn();
+
+    jest
+      .spyOn(React, 'useState')
+      .mockImplementationOnce((initState) => initState, setState);
+
+    const filterBrandMock = (e) => {
+      const filteredGuitars = guitars.filter((guitar) => {
+        if (e.target.textContent === guitar.brand) {
+          return guitar;
+        }
+      });
+      setState(filteredGuitars);
+    };
+    const user = userEvent.setup();
+
+    render(<FilterList guitars={guitars} filterBrand={filterBrandMock} />);
+
+    const schecter = screen.getByText('Schecter');
+    await act(async () => user.click(schecter));
+
+    expect(setState).toHaveBeenCalledWith([
+      {
+        brand: 'Schecter',
+        height: '85%',
+        model: 'Schecter Avenger Exotic - Spalted Maple',
+        price: 100,
+        width: '30%',
+      },
+    ]);
+
+    const ibanez = screen.getByText('Ibanez');
+    await act(async () => user.click(ibanez));
+    expect(setState).toHaveBeenCalledWith([
+      {
+        brand: 'Ibanez',
+        model: 'Ibanez AR520HFM-VLS - Violin Burst',
+        price: 300,
+        height: '100%',
+        width: '100%',
+      },
+      {
+        brand: 'Ibanez',
+        model: 'Ibanez AR520HFM-VLS - Violin Burst 2',
+        price: 300,
+        height: '100%',
+        width: '100%',
+      },
+    ]);
   });
 });
