cc3e0dfa1d1a93b03da94fc55eccfcf1c69cf1d0
Stalloyde <stalloyde93@gmail.com>
2023-07-09 15:14:10 +0800

    Add test

--- src/components/catalog/Card/Card.test.js
+++ src/components/catalog/Card/Card.test.js
@@ -5,13 +5,16 @@ import Card from './Card';
 import uniqid from 'uniqid';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
+import { Dialog } from '@mui/material';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
 
 describe('Card', () => {
   const guitar = {
     brand: 'PRS',
     model: 'PRS CE24 - Black',
     price: 100,
-    quantity: 0,
+    quantity: 1,
     id: uniqid(),
   };
 
@@ -87,6 +90,8 @@ describe('Card', () => {
 
   it('Added to Cart notification renders on button click', async () => {
     const setIsInCart = jest.fn();
+    const openMock = jest.fn();
+    const handleClickMock = jest.fn();
     const user = userEvent.setup();
 
     jest
@@ -94,17 +99,57 @@ describe('Card', () => {
       .mockImplementationOnce((isInCart) => [isInCart, setIsInCart]);
 
     render(
-      <>
-        {setIsInCart ? <div>Added to Cart</div> : <div>false</div>}
-        <div>
-          <button>Add To Cart</button>
-        </div>
-      </>
+      <div>
+        {setIsInCart ? (
+          <Dialog open={openMock}>
+            <div>
+              <DialogContent id='dialog-container'>
+                <DialogContentText id='dialog-content'>
+                  <strong>{guitar.model}</strong> has been added to cart
+                </DialogContentText>
+              </DialogContent>
+            </div>
+          </Dialog>
+        ) : null}
+        <button data-testid='test' onClick={handleClickMock}>
+          Add To Cart
+        </button>
+      </div>
     );
 
-    const button = screen.getByRole('button', { name: 'Add To Cart' });
+    const button = screen.getByTestId('test');
     await act(async () => user.click(button));
 
-    expect(screen.getByText('Added to Cart')).toBeInTheDocument();
+    expect(screen.getByText('has been added to cart')).toBeInTheDocument();
+  });
+
+  it('cart Array merges quantity for duplicate products instead of adding as separate object', async () => {
+    const setCartArray = jest.fn();
+    const handleClickMock = jest.fn(() => {
+      const inputValueMock = 4;
+      guitar.quantity += inputValueMock;
+    });
+    const user = userEvent.setup();
+
+    jest
+      .spyOn(React, 'useState')
+      .mockImplementationOnce((cartArray) => [cartArray, setCartArray]);
+
+    render(
+      <button data-testid='test' id={guitar.id} onClick={handleClickMock}>
+        Add To Cart
+      </button>
+    );
+
+    const button = screen.getByTestId('test');
+    await act(async () => user.click(button));
+
+    expect(guitar).toEqual({
+      brand: 'PRS',
+      model: 'PRS CE24 - Black',
+      price: 100,
+      quantity: 7,
+      id: guitar.id,
+    });
   });
 });
