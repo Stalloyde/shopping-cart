08f191de5e073ff86493c265a2a429e28e072ba0
Stalloyde <stalloyde93@gmail.com>
2023-07-09 22:17:58 +0800

    Add Cart Page: WIP - quantity input change & delete button

--- src/components/shopping-cart/Cart.js
+++ src/components/shopping-cart/Cart.js
@@ -1,11 +1,66 @@
 import React from 'react';
 import Header from '../header/Header';
+import './Cart.css';
+import { Link } from 'react-router-dom';
 
-const Cart = ({ cartArray }) => (
-  <>
-    <Header cartArray={cartArray} />
-    <div> SHOPPING CART HERE </div>
-  </>
+const CartGrids = ({ item }) => (
+  <div className='cart-content'>
+    <div className='cart-image-container'>
+      <img src={item.imageSrc} height={item.height} width={item.width}></img>
+    </div>
+    <div className='cart-details'>
+      <div>
+        <h3>{item.model}</h3>
+      </div>
+      <div>{`Price: $${item.price}`}</div>
+      <div>
+        Quantity:{' '}
+        <input type='number' defaultValue={item.quantity} min='1'></input>
+      </div>
+      <div>Subtotal: {`$${item.price * item.quantity}`}</div>
+    </div>
+
+    <div className='cart-delete'>
+      <button>Delete Item</button>
+    </div>
+  </div>
 );
+
+const Cart = ({ cartArray }) => {
+  const sum = () => {
+    const pricexQuantityArray = cartArray.map(
+      (item) => item.price * item.quantity
+    );
+    return pricexQuantityArray.reduce((total, current) => total + current);
+  };
+
+  return (
+    <>
+      <Header cartArray={cartArray} />
+      <div className='cart-container'>
+        {cartArray.length === 0 ? (
+          <>
+            Your shopping cart is currently empty.
+            <br />
+            <br />
+            <Link to='/catalog'>
+              <div>View Our Collection</div>
+            </Link>
+          </>
+        ) : (
+          <>
+            {cartArray.map((item) => (
+              <CartGrids item={item} key={item.model} />
+            ))}
+            <div className='checkout-container'>
+              <div className='total'>{`Total: $${sum()}`}</div>
+              <button>Check Out Cart</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Cart;
