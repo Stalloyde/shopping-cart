3e15597c94d74dd6ad8ecca1ef6435270add051e
Stalloyde <stalloyde93@gmail.com>
2023-07-10 10:40:44 +0800

    Enable editng of quantity

--- src/components/shopping-cart/Cart.js
+++ src/components/shopping-cart/Cart.js
@@ -3,30 +3,42 @@ import Header from '../header/Header';
 import './Cart.css';
 import { Link } from 'react-router-dom';
 
-const CartGrids = ({ item }) => (
-  <div className='cart-content'>
-    <div className='cart-image-container'>
-      <img src={item.imageSrc} height={item.height} width={item.width}></img>
-    </div>
-    <div className='cart-details'>
-      <div>
-        <h3>{item.model}</h3>
-      </div>
-      <div>{`Price: $${item.price}`}</div>
-      <div>
-        Quantity:{' '}
-        <input type='number' defaultValue={item.quantity} min='1'></input>
-      </div>
-      <div>Subtotal: {`$${item.price * item.quantity}`}</div>
-    </div>
+const CartGrids = ({ item, setQuantityToAddToCart }) => {
+  const handleChange = (e) => {
+    item.quantity = Number(e.target.value);
+    setQuantityToAddToCart(item.quantity);
+  };
 
-    <div className='cart-delete'>
-      <button>Delete Item</button>
-    </div>
-  </div>
-);
+  return (
+    <div className='cart-content'>
+      <div className='cart-image-container'>
+        <img src={item.imageSrc} height={item.height} width={item.width}></img>
+      </div>
+      <div className='cart-details'>
+        <div>
+          <h3>{item.model}</h3>
+        </div>
+        <div>{`Price: $${item.price}`}</div>
+        <div>
+          Quantity:{' '}
+          <input
+            type='number'
+            defaultValue={item.quantity}
+            min='1'
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div>Subtotal: {`$${item.price * item.quantity}`}</div>
+      </div>
 
-const Cart = ({ cartArray }) => {
+      <div className='cart-delete'>
+        <button>Delete Item</button>
+      </div>
+    </div>
+  );
+};
+
+const Cart = ({ cartArray, quantityToAddToCart, setQuantityToAddToCart }) => {
   const sum = () => {
     const pricexQuantityArray = cartArray.map(
       (item) => item.price * item.quantity
@@ -49,9 +61,17 @@ const Cart = ({ cartArray }) => {
           </>
         ) : (
           <>
-            {cartArray.map((item) => (
-              <CartGrids item={item} key={item.model} />
-            ))}
+            <div className='cart-grids-container'>
+              {cartArray.map((item) => (
+                <CartGrids
+                  item={item}
+                  key={item.model}
+                  id={item.id}
+                  quantityToAddToCart={quantityToAddToCart}
+                  setQuantityToAddToCart={setQuantityToAddToCart}
+                />
+              ))}
+            </div>
             <div className='checkout-container'>
               <div className='total'>{`Total: $${sum()}`}</div>
               <button>Check Out Cart</button>
