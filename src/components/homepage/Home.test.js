87d3cc89b2fc345508c7180857255e242930bbb4
Stalloyde <stalloyde93@gmail.com>
2023-07-10 21:44:05 +0800

    Simplify test

--- src/components/homepage/Home.test.js
+++ src/components/homepage/Home.test.js
@@ -2,28 +2,20 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Router, MemoryRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import { Link } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
-import Home from './Home';
 
 describe('Home component', () => {
-  it('renders the hompeage', () => {
-    render(
-      <MemoryRouter>
-        <Home />
-      </MemoryRouter>
-    );
-
-    expect(screen.getByTestId('homepage')).toMatchSnapshot();
-  });
-
   it('change current path to catalog when button is clicked', async () => {
     const user = userEvent.setup();
     const history = createMemoryHistory({ initialEntries: ['/'] });
     render(
       <Router location={history.location} navigator={history}>
-        <Home />
+        <Link to='/catalog'>
+          <div>View Our Collection</div>
+        </Link>
       </Router>
     );
     expect(history.location.pathname).toBe('/');
