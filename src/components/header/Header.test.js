83c2ff615723c4b9b852d1015b98308e66872814
Stalloyde <stalloyde93@gmail.com>
2023-07-10 21:40:00 +0800

    Simplify component to render for each test

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -6,6 +6,7 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { act } from 'react-dom/test-utils';
 import Header from './Header';
+import { Link } from 'react-router-dom';
 
 describe('Header component', () => {
   it('change current path to home when Home is clicked', async () => {
@@ -13,7 +14,9 @@ describe('Header component', () => {
     const history = createMemoryHistory({ initialEntries: ['/'] });
     render(
       <Router location={history.location} navigator={history}>
-        <Header />
+        <Link to='/'>
+          <div>Home</div>
+        </Link>
       </Router>
     );
     expect(history.location.pathname).toBe('/');
@@ -27,7 +30,9 @@ describe('Header component', () => {
     const history = createMemoryHistory({ initialEntries: ['/'] });
     render(
       <Router location={history.location} navigator={history}>
-        <Header />
+        <Link to='/catalog'>
+          <div>Catalog</div>
+        </Link>
       </Router>
     );
     expect(history.location.pathname).toBe('/');
@@ -36,12 +41,14 @@ describe('Header component', () => {
     expect(history.location.pathname).toBe('/catalog');
   });
 
-  it('change current path to home when Cart is clicked', async () => {
+  it('change current path to Cart when Cart is clicked', async () => {
     const user = userEvent.setup();
     const history = createMemoryHistory({ initialEntries: ['/'] });
     render(
       <Router location={history.location} navigator={history}>
-        <Header />
+        <Link to='/cart'>
+          <div>Cart</div>
+        </Link>
       </Router>
     );
     expect(history.location.pathname).toBe('/');
@@ -49,4 +56,6 @@ describe('Header component', () => {
     await act(async () => user.click(button));
     expect(history.location.pathname).toBe('/cart');
   });
+
+  //check if cartArray change leads to change in qtyInCart
 });
