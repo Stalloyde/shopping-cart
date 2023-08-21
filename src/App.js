1a3ca9ee655b12cf8363374a532d15cf8d614b01
Stalloyde <stalloyde93@gmail.com>
2023-08-21 11:25:31 +0800

    Change router type

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Catalog from './components/catalog/Catalog';
 import Home from './components/homepage/Home';
 import Cart from './components/shopping-cart/Cart';
@@ -10,32 +10,30 @@ const App = () => {
   const [quantityToAddToCart, setQuantityToAddToCart] = useState(1);
 
   return (
-    <HashRouter>
-      <Routes>
-        <Route path='/' element={<Home cartArray={cartArray} />} />
-        <Route
-          path='/catalog'
-          element={
-            <Catalog
-              cartArray={cartArray}
-              setCartArray={setCartArray}
-              quantityToAddToCart={quantityToAddToCart}
-              setQuantityToAddToCart={setQuantityToAddToCart}
-            />
-          }
-        />
-        <Route
-          path='/cart'
-          element={
-            <Cart
-              cartArray={cartArray}
-              setCartArray={setCartArray}
-              setQuantityToAddToCart={setQuantityToAddToCart}
-            />
-          }
-        />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path='/' element={<Home cartArray={cartArray} />} />
+      <Route
+        path='/catalog'
+        element={
+          <Catalog
+            cartArray={cartArray}
+            setCartArray={setCartArray}
+            quantityToAddToCart={quantityToAddToCart}
+            setQuantityToAddToCart={setQuantityToAddToCart}
+          />
+        }
+      />
+      <Route
+        path='/cart'
+        element={
+          <Cart
+            cartArray={cartArray}
+            setCartArray={setCartArray}
+            setQuantityToAddToCart={setQuantityToAddToCart}
+          />
+        }
+      />
+    </Routes>
   );
 };
 
