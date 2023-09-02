03d06a8c927bbc9fc07e203a094f05eff5681763
Stalloyde <stalloyde93@gmail.com>
2023-09-02 14:11:16 +0800

    Add mobile drop drop menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,74 +1,85 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../App';
-import menuIcon from '../../../public/menu-icon.svg';
-
-const Nav = ({ homeNav = 'homeNav' }) => {
-  const { cartArray } = useContext(CartContext);
-
-  const qtyInCart = () => {
-    if (cartArray.length > 0) {
-      const qtyArray = cartArray.map((item) => item.quantity);
-      return qtyArray.reduce((total, current) => total + current);
-    }
-
-    return 0;
-  };
-  return (
-    <>
-      <Link className={styles[homeNav]} to='/'>
-        <div>Home</div>
-      </Link>
-      <Link className={styles[homeNav]} to='/catalog/all products'>
-        <div>Catalog</div>
-      </Link>
-      <Link className={styles[homeNav]} to='/cart'>
-        <div>Cart</div>
-        <div className={styles.cartArray}>{qtyInCart()}</div>
-      </Link>
-    </>
-  );
-};
-
-const DropDownMenu = ({ homeNav = 'homeNav' }) => {
-  const { cartArray } = useContext(CartContext);
-
-  const qtyInCart = () => {
-    if (cartArray.length > 0) {
-      const qtyArray = cartArray.map((item) => item.quantity);
-      return qtyArray.reduce((total, current) => total + current);
-    }
-
-    return 0;
-  };
-
-  return (
-    <div className={styles.navMobile}>
-      asdasdasds
-      <Link className={styles[homeNav]} to='/'>
-        <div>Home</div>
-      </Link>
-      <Link className={styles[homeNav]} to='/catalog/all products'>
-        <div>Catalog</div>
-      </Link>
-      <Link className={styles[homeNav]} to='/cart'>
-        <div>Cart</div>
-        <div className={styles.cartArray}>{qtyInCart()}</div>
-      </Link>
-    </div>
-  );
-};
+import menuIcon from '/menu-icon.svg';
 
 const Header = () => {
+  const [isMenuClick, setMenuClick] = useState(false);
+  const { cartArray } = useContext(CartContext);
+
+  const qtyInCart = () => {
+    if (cartArray.length > 0) {
+      const qtyArray = cartArray.map((item) => item.quantity);
+      return qtyArray.reduce((total, current) => total + current);
+    }
+
+    return 0;
+  };
+
+  const NavWeb = ({ homeNav = 'homeNav' }) => {
+    return (
+      <div className={styles.navWeb}>
+        <Link className={styles[homeNav]} to='/'>
+          <div>Home</div>
+        </Link>
+        <Link className={styles[homeNav]} to='/catalog/all products'>
+          <div>Catalog</div>
+        </Link>
+        <Link className={styles[homeNav]} to='/cart'>
+          <div>Cart</div>
+          <div className={styles.cartArray}>{qtyInCart()}</div>
+        </Link>
+      </div>
+    );
+  };
+
+  const DropDownMenu = () => {
+    return (
+      <nav className={styles.dropDownMenuNav}>
+        <Link
+          className={styles.mobileLink}
+          to='/'
+          onClick={() => {
+            setMenuClick(!isMenuClick);
+          }}
+        >
+          Home
+        </Link>
+        <Link
+          className={styles.mobileLink}
+          to='/catalog/all products'
+          onClick={() => {
+            setMenuClick(!isMenuClick);
+          }}
+        >
+          Catalog
+        </Link>
+        <Link
+          className={styles.mobileLink}
+          to='/cart'
+          onClick={() => {
+            setMenuClick(!isMenuClick);
+          }}
+        >
+          Cart <span className={styles.cartArray}>{qtyInCart()}</span>
+        </Link>
+      </nav>
+    );
+  };
+
   return (
     <header>
       <div className={styles.storeName}>
         <h1>Tech & Threads</h1>
       </div>
       <nav>
-        <Nav />
-        <img src={menuIcon} className={styles.navMobile}></img>
+        <NavWeb />
+        <div className={styles.navMobile}>
+          <img src={menuIcon} onClick={() => setMenuClick(!isMenuClick)}></img>
+          <div className={styles.cartArray}>{qtyInCart()}</div>
+          {isMenuClick ? <DropDownMenu /> : null}
+        </div>
       </nav>
     </header>
   );
